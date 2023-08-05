f06b36560f2223d22619e818eae4fe09fa8a2819
amadordavila505 <proalexander5@gmail.com>
2023-08-05 08:05:17 -0600

    correcion de errores de indentacion

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,15 +1,12 @@
 import {Module} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './dto/product.dto';
 import { product } from '../user/user.entity';
-import { ProductModule } from './products.module';
 
 @Module({
     imports: [TypeOrmModule.forFeature([product])],
-    provider: [],
-    Controller:[],
-    
-    export class ProductModule{}
+    providers: [],
+    controllers:[],
+
 })
 
-export { ProductModule };
+export class ProductModule {};
